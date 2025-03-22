2b13faf000ddd9fb871c4fec2f41705c0a288795
Salma-Yousry <salmayousry779@gmail.com>
2025-03-23 01:16:38 +0200

    auth login and register

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:app_todo/app_color.dart';
+import 'package:app_todo/home/login/login_screen.dart';
 import 'package:app_todo/home/settings/settings_tab.dart';
 import 'package:app_todo/home/task_list/add_task_button_sheet.dart';
 import 'package:app_todo/home/task_list/task_list_tab.dart';
+import 'package:app_todo/providers/auth_user_provider.dart';
+import 'package:app_todo/providers/listprovider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String route_name = 'Home_Screen';
@@ -15,9 +19,21 @@ class _HomeScreenState extends State<HomeScreen> {
   int itemindex=0;
   @override
   Widget build(BuildContext context) {
+    var authprovider = Provider.of<AuthUserProvider>(context);
+    var listprovider = Provider.of<ListProvider>(context);
     return Scaffold(
       appBar: AppBar(
-    title: Text('To Do List',style: Theme.of(context).textTheme.titleLarge,),
+    title: Text('To Do List{${authprovider.currentUser!.name!}}'
+      ,style: Theme.of(context).textTheme.titleLarge,),
+      actions: [
+        IconButton(onPressed: (){
+listprovider.taskList=[];
+//ملهاش لزمة عشن احنا عاملين في ال  AuthUserProvider ان   currentUser = newUser; كده كده بيجدده
+///authprovider.currentUser=null;
+          ///
+          Navigator.of(context).pushReplacementNamed(LoginScreen.route_name);
+        }, icon: Icon(Icons.logout))
+      ],
       // toolbarHeight: MediaQuery.of(context).size.height*0.14,
       ),
       bottomNavigationBar:BottomAppBar(
--- lib/model/task.dart
+++ lib/model/task.dart
@@ -1,11 +1,11 @@
 class Task {
   static const String collectionName = 'tasks';
 
-  String? id;
-  String? title;
-  String? description;
-  DateTime? dateTime;
-  bool? isDone;
+  String id;
+  String title;
+  String description;
+  DateTime  dateTime;
+  bool isDone;
 
   Task(
       {this.id = '', required this.title, required this.description, required this.dateTime, this.isDone = false});
@@ -28,7 +28,7 @@ class Task {
       'title': title,
       'description': description,
       ///millisecondsSinceEpoch نخزنه integer
-      'dateTime': dateTime!.millisecondsSinceEpoch,
+      'dateTime': dateTime.millisecondsSinceEpoch,
       'isDone': isDone
     };
   }
